71ce26f38492bd4f35b427aa0e12de4108fc89f6
krcoombes <krcoombes@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-05-26 12:25:18 +0000

    Commented/questioned Algorithm_new-v3.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/clonefinder@35 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- Algorithms/Algorithm_new-v3.R
+++ Algorithms/Algorithm_new-v3.R
@@ -12,42 +12,47 @@ start <- t(sapply(1:nrow(cset), function(i){sort(cset[i,]/sum(cset[i,]))}))
 x <- 1:1000
 
 f <- function(sim){
+  # load the simulated data set
   set <- callfun(sim)
-  truepsis <- set[[1]]
-  phiset <- set[[2]]
-  zeds <- set[[3]]  
-  lengths <- sapply(1:1000, function(i){length(which(phiset[i,]>=.05))})
+  truepsis <- set[[1]] # omniscient
+  phiset <- set[[2]]   # previously estimated phi from the observed data
+  zeds <- set[[3]]     # omniscient
+  lengths <- sapply(1:1000, function(i){length(which(phiset[i,]>=.05))}) # putative number of clones per segment
   subsets <-list(which(lengths==1),
                  which(lengths==2),
                  which(lengths==3),
                  which(lengths==4),
-                 which(lengths==5))
-  nclones <- length(truepsis)
-  nonones <- phiset[sort(unique(c(subsets[[2]], subsets[[3]], subsets[[4]], subsets[[5]]))),]
-  i.non <- sort(c(subsets[[2]], subsets[[3]], subsets[[4]], subsets[[5]]))
-  i.one <- subsets[[1]]
+                 which(lengths==5)) # why this data structure/algorithm?
+  nclones <- length(truepsis) # omniscient
+  nonones <- phiset[sort(unique(c(subsets[[2]], subsets[[3]], subsets[[4]], subsets[[5]]))),] # informative
+  i.non <- sort(c(subsets[[2]], subsets[[3]], subsets[[4]], subsets[[5]]))                # non-informative
+  i.one <- subsets[[1]] # index of non-informative segments
   foo <- function(v){
-    combos <- sort(unique(unlist(sapply(1:length(v), function(i){combn(v, i, sum)}))))
-    m <- t(sapply(1:length(v), function(i){sort(c(v[i], 1-v[i]), decreasing=TRUE)}))
+    combos <- sort(unique(unlist(sapply(1:length(v), function(i){combn(v, i, sum)})))) # expand psis to sums
+    m <- t(sapply(1:length(v), function(i){sort(c(v[i], 1-v[i]), decreasing=TRUE)}))   # what is this?
     rowmatch <- function(row){
+      # what is thsi fiunction supposed to do? Inputs? outputs? goal?
+
+      # next line looks like a complicated way to compute lengths[row]. am I missing something?
       l <- which(sapply(1:length(subsets), function(i){(row %in% subsets[[i]])}))
       resfun <- function(j){
-        if(l>=3){
+        # note: this function only gets called when l < 3. Why another if-else?
+        if(l >= 3){ # it's probably bad practice to keep relying on external vraiables instead of passing them in as arguments
           vec <- sort(v, decreasing=TRUE)
-        }else{
+        }else{ # what is vec for? and whow does m come into it?
           vec <- m[j,]
         }
         
-        res <- sum((sort(phiset[row,], decreasing=TRUE)[1:length(vec)] - vec)^2)
-        if(l>length(vec)){
-          res <- res + sum((sort(phiset[row,], decreasing=TRUE)[(length(vec)+1):l])^2)
+        res <- sum((sort(phiset[row,], decreasing=TRUE)[1:length(vec)] - vec)^2) # sum of squared errors?
+        if(l > length(vec)){
+          res <- res + sum((sort(phiset[row,], decreasing=TRUE)[(length(vec)+1):l])^2) # ???
         }
         res
       }
       mat <- NULL
-      if(l>=3){
+      if(l >= 3){
         pick <- sort(v, decreasing=TRUE)
-        mat <- match(sort(phiset[row,], decreasing=TRUE)[1:length(v)], phiset[row,])
+        mat <- match(sort(phiset[row,], decreasing=TRUE)[1:length(v)], phiset[row,]) # what are we matchinjg?
       }else{
         pick <- m[which.min(sapply(1:nrow(m), resfun)),]
         mat <- match(sort(phiset[row,], decreasing=TRUE)[1:l], phiset[row,])
