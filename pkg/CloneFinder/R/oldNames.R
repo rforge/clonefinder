2d76725cb7bb11e8f6851a1f66488b328647ea49
krcoombes <krcoombes@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-10-22 14:36:33 +0000

    Even more ruthlessly took a machete to "functions.R' to hack out the undergrowth.
    Note that all the removed code is temporarily stored in the repository but *above* the "pkg" directory, in extras/obsolete.R.
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/clonefinder@96 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/CloneFinder/R/oldNames.R
@@ -0,0 +1,13 @@
+### The first three functions are here for backwards compatibility.
+### They are officially "deprecated", which means (a) you should not
+### use them in new code because (b) at some point theu will simply
+### go away.
+tumorGen <- function(...) {
+  tumor <- Tumor(...)
+  as(tumor, "list")
+}
+
+dataGen <- function(tumor, ...) {
+  generateTumorData(as(tumor, "Tumor"), ...)
+}
+
